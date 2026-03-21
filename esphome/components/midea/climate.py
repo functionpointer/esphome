f460ee788c1744831c394df8d856962a1a4c7cc4
functionpointer <suspendfunction@gmail.com>
2026-03-21 14:23:06 +0100

    midea: Use functionpointer version of library

--- esphome/components/midea/climate.py
+++ esphome/components/midea/climate.py
@@ -295,4 +295,8 @@ async def to_code(config):
     # MideaUART library requires WiFi (WiFi auto-enables Network via dependency mapping)
     if CORE.is_esp32:
         cg.add_library("WiFi", None)
-    cg.add_library("dudanov/MideaUART", "1.1.9")
+    cg.add_library(
+        name="MideaUART",
+        version=None,
+        repository="https://github.com/functionpointer/MideaUART.git",
+    )
